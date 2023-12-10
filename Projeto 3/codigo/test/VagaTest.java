2d0e372b2a3ee48f31f6c15fe5061175317d15dd
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-12-10 19:57:55 -0300

    add testes

---
+++ Projeto 3/codigo/test/VagaTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class VagaTest {
+    private Vaga vaga;
+
+    @Before
+    public void setUp() {
+        vaga = new Vaga("V1");
+    }
+
+    @Test
+    public void testEstacionarVagaDisponivel() {
+        assertTrue(vaga.estacionar());
+    }
+
+    @Test
+    public void testEstacionarVagaOcupada() {
+        vaga.estacionar();
+        assertFalse(vaga.estacionar());
+    }
+
+    @Test
+    public void testSairVagaOcupada() {
+        vaga.estacionar();
+        assertTrue(vaga.sair());
+    }
+
+    @Test
+    public void testSairVagaDisponivel() {
+        assertTrue(vaga.disponivel());
+        assertFalse(vaga.sair());
+    }
+}
